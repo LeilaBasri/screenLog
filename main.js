117ee73f101b74f6699a41c8f4f6c8d3869db9d2
Leila Basri <33898821+LeilaBasri@users.noreply.github.com>
2023-11-22 16:20:13 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function samFunct(el) {
+var x = el.clientX;
+var y = el.clientY;
+var coo = "Coordinates are: (" + x + "," + y + ")";
+document.getElementById("demo").innerHTML = coo;
+}
+function clearCoo() {
+document.getElementById("demo").innerHTML = "Keep mouse pointer within";
+}
